5fbf9b51a91d184feb7e0763d8e32c6e263e6916
diana-91 <diana.hervasmoya@gmail.com>
2018-01-17 22:41:28 +0100

    iteration 3: step 1

--- starter_code/express-spotify-app/app.js
+++ starter_code/express-spotify-app/app.js
@@ -1,4 +1,14 @@
 var SpotifyWebApi = require('spotify-web-api-node');
+const express = require('express');
+const app = express();
+const bodyParse = require('body-parser');
+const morgan = require('morgan');
+const expressLayouts = require('express-ejs-layouts');
+
+app.use(bodyParse.json());
+app.use(bodyParse.urlencoded({extended: true}));
+app.set('views', __diname + '/views');
+app.set('view engine','ejs');
 
 // Remember to paste here your credentials
 var clientId = '1c30624cba6742dcb792991caecae571',
@@ -16,3 +26,13 @@ spotifyApi.clientCredentialsGrant()
   }, function(err) {
     console.log('Something went wrong when retrieving an access token', err);
 });
+
+app.get('/', (req, res, next) => {
+  res.render('index');
+});
+
+app.post('artist', (req, res, next) =>{
+  res.render(req.body.artist);
+});
+
+app.listen(3000, console.log('server started'));
